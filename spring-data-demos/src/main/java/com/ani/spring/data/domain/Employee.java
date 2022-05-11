7ac27d85aa80691445ed205b2a51aff9f335507d
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-05-11 16:09:53 +0530

    Spring Data : Cascading, Fetch Type

--- spring-data-demos/src/main/java/com/ani/spring/data/domain/Employee.java
+++ spring-data-demos/src/main/java/com/ani/spring/data/domain/Employee.java
@@ -4,10 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -20,7 +19,11 @@ public class Employee {
     private String name;
     private LocalDate dob;
 
-    @OneToMany(mappedBy = "employee")
+    @OneToMany(
+            mappedBy = "employee",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
 //    @OneToMany
-    private List<Mobile> mobiles;
+    private List<Mobile> mobiles = new ArrayList<>();
 }
